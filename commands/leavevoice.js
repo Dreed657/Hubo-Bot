aaf768397b0362b777dd313dbda0dc6361a0fa9e
DreddBG <ninjakiviWTF@gmail.com>
2019-07-28 22:17:38 +0300

    Fixes for voice commands.

--- commands/leavevoice.js
+++ commands/leavevoice.js
@@ -1,24 +1,15 @@
 exports.run = (client, message, args) => {
     message.delete();
-
-    //
-    //NOT WORKING 
-    //
-
     const botChannel = client.channels.get('603003319816028162');
+    const VC = client.channels.get('159011396909137921');
 
-    if(message.member.voiceChannel) {
-        //let currentChannel = client.voiceConnections.map(voiceConnection => console.log(voiceConnection.channel.id));
-        try {
-            //console.log(message.member.channel.id);
-            //client.leaveVoiceChannel(message.member.voiceState.channelID);
-            console.log(message.voiceChannel);
-        } catch (err) {
-            botChannel.send('Something went south.');
-            console.log(err)
-        }
+    try {
+        client.leaveVoiceChannel(message.author.voiceChannel);
+    } catch (err) {
+        botChannel.send('Something went south.');
+        console.log(err)
     }
-       
+
     };
 
 exports.help = {
--- commands/voice.js
+++ commands/voice.js
@@ -1,11 +1,9 @@
-const fs = require('fs');
 const ytdl = require('ytdl-core');
 
 exports.run = (client, message, args) => {
     message.delete();
 
-    const botChannel = client.channels.get('603003319816028162');
-    
+    var VC = message.member.voiceChannel;
     if (!VC)
         return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
     VC.join()
@@ -19,24 +17,8 @@ exports.run = (client, message, args) => {
             } catch (err) {
                 console.log(err)
             }
-
-
-           
         })
         .catch(console.error);
-
-
-
-
-    // if(message.member.voiceChannel) {
-    //     try {
-    //         message.member.voiceChannel.join();
-    //     } catch (err) {
-    //         botChannel.send('Something went south.');
-    //         console.log(err)
-    //     }
-    // }
-       
     };
 
 exports.help = {
